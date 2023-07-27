1293ce1deb6d10f3dd4119df87bd8f431e953673
☆ Asfoxyre <maffichu@gmail.com>
2023-07-27 08:57:01 +0300

    fb

--- team6/src/main.js
+++ team6/src/main.js
@@ -4,3 +4,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjGTbcmAtcRFVhJ4sbe88ht9bln0O2Ork",
+  authDomain: "echipa6-11d55.firebaseapp.com",
+  projectId: "echipa6-11d55",
+  storageBucket: "echipa6-11d55.appspot.com",
+  messagingSenderId: "538418421472",
+  appId: "1:538418421472:web:af167b13a246526cb9ba85",
+  measurementId: "G-KFNGG6257S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
